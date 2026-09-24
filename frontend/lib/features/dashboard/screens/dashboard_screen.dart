49405d382cfb94b6bf14754077146985d83866f0
Sourabh Kumar Patel <74050218+sourabh2310@users.noreply.github.com>
2026-09-24 17:28:56 +0000

    UI error fix

--- frontend/lib/features/dashboard/screens/dashboard_screen.dart
+++ frontend/lib/features/dashboard/screens/dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' hide TextDirection;
 
 import '../../app_scope.dart';
 import '../../preferences/shell_destinations.dart';
